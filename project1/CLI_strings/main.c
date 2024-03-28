6745bda02cc8180e973071ae9e07a218e94df922
Jan Burbicki <jan.burbicki@gmail.com>
2024-03-28 01:52:10 +0100

    Finished 1st iteration of main function

--- project1/CLI_strings/main.c
+++ project1/CLI_strings/main.c
@@ -3,24 +3,36 @@
 
 int main(int argc, char *argv[2]) {
 
+	/*variables declaraion*/
+	FILE* file;
+	char read_buff[BUFF_SIZE];
+	char delimiter[DELIMITER_SIZE];
+
 	/*handling main arguments*/
 	if (argc != 2) {
 		printf("Wrong number of arguments, expected 2\n");
 		return EXIT_FAILURE;
 	}
 
-	/*variables declaraion*/
-	FILE* file;
-	char read_buff[BUFF_SIZE];
+	/*removing "" from delimiter's string*/
+	delimiter[0] ="";
+	delimiter[sizeof(delimiter-1)] ="";
 
-
-	/*getting file to programs memory*/
+	/*getting file to program's memory*/
 	file = fopen(argv[0], "r");
 	if (file == NULL) {
 		printf("File opening error!\n");
 		return EXIT_FAILURE;
 	}
+	fgets(read_buff, BUFF_SIZE, file);
+	fclose(file);
 
+	/*spliting strings to separate lines*/
+	char* token = strtok(read_buff, delimiter);
+	while (token != NULL) {
+		printf("%s\n", token);
+		token = strtok(NULL, delimiter);
+	}
 
 	return EXIT_SUCCESS;
 
--- project1/CLI_strings/main.h
+++ project1/CLI_strings/main.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BUFF_SIZE 1024
+#define DELIMITER_SIZE 4
