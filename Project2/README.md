2ddd59a8740fa39cb0e70192949eb07a1670429f
OstrichDowneyJr <25938528+OstrichDowneyJr@users.noreply.github.com.>
2024-04-03 19:07:54 +0200

    test revert

---
+++ Project2/README.md
@@ -0,0 +1,19 @@
+Create a simple file traversal script which takes a path to the directory as an input.
+```
+NAME
+       traverse - list the files inside (but not directories) inside a folder.
+
+SYNOPSIS
+       cp [-r] [-m] ROOT_DIR
+
+DESCRIPTION
+       -r, --recursive
+              Also list files in subfolders.
+        -m, --metadata
+              Also show files metadata.
+
+```
+
+Notes:
+
+ - metadata have to show at least the size of the files.
--- Project2/main.c
+++ Project2/main.c
@@ -1,4 +1,54 @@
-#include "main.h" 
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int ListDirectoryContents(const char* sDir){
+
+    WIN32_FIND_DATA fdFile;
+    HANDLE hFind = NULL;
+
+    char sPath[2048];
+
+    sprintf(sPath, "%s\\*.txt", sDir);
+    if ((hFind = FindFirstFile(sPath, &fdFile)) == INVALID_HANDLE_VALUE){
+        printf("Path not found: [%s]\n", sDir);
+        return 1;
+    }
+    do{
+        if(strcmp(fdFile.cFileName, ".") != 0 && strcmp(fdFile.cFileName, "..") != 0){
+            sprintf(sPath, "%s\\%s", sDir, fdFile.cFileName);
+
+            if (fdFile.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY){
+                printf("Directory: %s\n", sPath);
+                ListDirectoryContents(sPath);
+            }
+            else {
+                printf("File: %s\n", sPath);
+            }
+        }
+    }
+    while (FindNextFile(hFind, &fdFile)); 
+    FindClose(hFind); 
+    return 0;
+}
+
+void traverse(char* path) {
+
+	FILE* file; 
+    char buffer;
+
+	file = fopen(path, "r");
+    buffer = fgetc(file);
+
+    while (buffer != EOF)
+    {
+        printf("%c", buffer);
+        buffer = fgetc(file);
+    }
+    fclose(file);
+
+}
+
 
 int main(int argc, char** argv) {
 
