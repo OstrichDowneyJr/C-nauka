a088f7eebfef7d351e87ac88015bbaed3ae9a978
OstrichDowneyJr <25938528+OstrichDowneyJr@users.noreply.github.com.>
2024-04-03 19:03:06 +0200

    Revert "Finished on PR edits and expanded on tests"
    This reverts commit 0f19b4ddd65d5baf5a0080af08f949c8237010e7.

--- project1/CLI_strings/main.c
+++ project1/CLI_strings/main.c
@@ -1,55 +1,31 @@
-#pragma warning(disable : 4994)
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define _CRT_SECURE_NO_WARNINGS
-#define __STDC_WANT_LIB_EXT0__ 1
-#define _GNU_SOURCE 
-#define BUFF_SIZE 1023
-#define DELIMITER_SIZE 3
+#include "main.h"
 
 
-int split_file(char* path_to_txt, char* delimters);
+int main(int argc, char *argv[]) {
 
-int main(int argc, char** argv) {
-
-	printf("Testing function\n");
-	int a = split_file("Dummy.txt", ":");
-	int b = split_file("Dummy2.txt",";");
-	int c = split_file("Dummy3.txt",".");
-
-	printf("Test a:%d \t Test b:%d\t Test c:%d\n", a, b, c);
-	printf("Test is successful if x=0");
-	return EXIT_SUCCESS;
-}
-
-
-int split_file(char* path_to_txt, char* delimiters) {
-
-
-	FILE* file;
-	char read_buff[BUFF_SIZE];
-	char* current_section;
-
-
-	if ((file = fopen(path_to_txt, "r")) == NULL) {
-		printf("File opening error!\n");
-		printf("Tried to open %s\n",path_to_txt);
+	///*handling main arguments*/
+	if (argc != 3) {
+		printf("Wrong number of arguments, expected 2\n");
 		return EXIT_FAILURE;
 	}
+	FILE* file;
+	/*variables declaraion*/
+	char read_buff[BUFF_SIZE];
 
+	/*getting file to program's memory*/
+	if ((file = fopen(argv[1], "r")) == NULL) {
+		printf("File opening error!\n");
+		return EXIT_FAILURE;
+	}
 	fgets(read_buff, BUFF_SIZE,file);
 	fclose(file);
-	printf("Raw version of the text file is:\n");
-	printf("%s\n", read_buff);
-	printf("Edited version of the text file is:\n");
-
-
-	current_section= strtok(read_buff,delimiters);
-	while (current_section != NULL) {
-		printf("%s\n", current_section);
-		current_section = strtok(NULL,delimiters); 
+	/*spliting strings to separate lines*/
+	char* token;
+	/*to nie dziala, nie umiem zapisac tego wskaznika*/
+	token= strtok(read_buff,argv[2]);
+	while (token != NULL) {
+		printf("%s\n", token);
+		token = strtok(NULL,argv[2]); /*tu tez*/
 	}
 
 	return EXIT_SUCCESS;
