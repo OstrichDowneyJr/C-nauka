58cda43700055700f3b490fb255c3091ae8604d9
OstrichDowneyJr <>
2025-06-07 22:26:13 +0200

    Safe code achived sir

--- Project2/main.c
+++ Project2/main.c
@@ -1,49 +1,49 @@
-/**
- * C program to list contents of a directory recursively.
- */
-
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
 
-void listFiles(char *path);
+int listFiles(const char* rootDir);
 
+#define BUFF_MAX 100
 
-int main(int argc, char *argv[]){
-    char path[100];
-    printf("Enter path to list files: ");
-    scanf("%s", path);
+int main(const int argc, char *argv[]) {
+    char path[BUFF_MAX];
+    if (argc >= 2) {
+        strncpy(path, argv[1], BUFF_MAX - 1);
+        path[BUFF_MAX - 1] = '\0';  // Ensure null termination
+    } else if (fgets(path, BUFF_MAX, stdin) != NULL) {
+        path[strcspn(path, "\n")] = 0;
+    } else {
+        printf("No path provided\n");
+        return 1;
+    }
 
     listFiles(path);
 
     return 0;
 }
 
-
-void listFiles(char *basePath)
-{
-    char path[1000];
+int listFiles(const char* rootDir) { // NOLINT(*-no-recursion)
     struct dirent *dp;
-    DIR *dir = opendir(basePath);
-
+    DIR *dir = opendir(rootDir);
+    
     if (!dir) {
-        return;
+        return 1;
     }
 
-    while ((dp = readdir(dir)) != NULL)
-    {
-        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
-        {
+    while ((dp = readdir(dir)) != NULL) {
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0) {
             printf("%s\n", dp->d_name);
-
-            // Construct new path from our base path
-            strcpy(path, basePath);
-            strcat(path, "/");
-            strcat(path, dp->d_name);
-
-            listFiles(path);
+            
+            char newPath[BUFF_MAX];
+            strncpy(newPath, rootDir, BUFF_MAX - 1);
+            strncat(newPath, "/", BUFF_MAX - strlen(newPath) - 1);
+            strncat(newPath, dp->d_name, BUFF_MAX - strlen(newPath) - 1);
+            
+            listFiles(newPath);
         }
     }
 
     closedir(dir);
+    return 0;
 }
